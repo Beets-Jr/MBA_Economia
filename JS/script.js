e6730c3b555693a9f87ee24744598979ac79eb63
Mun1nm <joaoaveraldo@yahoo.com.br>
2024-08-19 16:07:04 -0300

    Adicionado a primeira versao dos Slides para mobile e desktop

--- JS/script.js
+++ JS/script.js
@@ -7,4 +7,39 @@ document.addEventListener('DOMContentLoaded', function () {
         this.classList.toggle('active');
       });
     });
-  });
+});
+
+// Swiper JS
+let swiperCards = new Swiper('.sbr-card-content', {
+  loop: false,
+  spaceBetween: 32,
+  grabCursor: true,
+  centeredSlides: true,
+
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  breakpoints: {
+    600: {
+      slidesPerView: 2,
+    },
+    900: {
+      slidesPerView: 3,
+    },
+    1200: {
+      slidesPerView: 4,
+    },
+    1500: {
+      slidesPerView: 5,
+    },
+  },
+
+  slideActiveClass: 'swiper-slide-active',
+});
